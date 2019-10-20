96a5a516d18e39173a9ef2292d28cacb04858dab
Konstantinov A <constantinov.al@yandex.ru>
2019-10-20 17:37:12 +0300

    1

--- stl_lib.py
+++ stl_lib.py
@@ -215,38 +215,5 @@ def read_stl(fname):
     return rez
 
 
-def group_by(data, by, tol=1e-6):
-    tmp = data.copy()
-    tmp.sort(order=by)
-    ii = [0] + (np.where(np.diff(tmp[by]) >= tol)
-                [0]+1).tolist() + [len(tmp[by])]
-    groups = []
-    for i in range(len(ii)-1):
-        groups.append(tmp[ii[i]:ii[i+1]])
-    return groups
-
-
 if __name__ == '__main__':
-    import matplotlib.pylab as plt
-    from itertools import cycle
-    import vtkplotter as vtk
-    p = vtk.Plotter(shape=(1, 2), bg='white', axes=1)
-    a = vtk.load('1.vtk')
-    sc = [c[1] for c in a.getPoints()]
-    a.pointColors(sc)
-    b = a.ybounds()
-    i = a.isolines(20, b[0], b[1])
-    i2 = a.isolines(20, b[1]*0.7, b[1]).clean()
-    ii = i.clean()
-    pnts1 = np.array([tuple(p) for p in ii.getPoints()], dtype=[
-        ('x', float), ('y', float), ('z', float)])
-    g1 = group_by(pnts1, by='y', tol=1e-3)
-    p.show(a, i, i2, at=0, N=2)
-    p.show(vtk.Points(ii.getPoints()), vtk.Points(i2.getPoints()),
-           vtk.Points(g1[10].tolist(), c='green'),
-           vtk.Points(g1[0].tolist(), c='red'), at=1, interactive=True)
-    for g in g1:
-        plt.plot(g['x'], g['z'], '.')
-    plt.grid()
-    plt.gca().set_aspect('equal', 'box')
-    plt.show()
+    pass
--- usefull_features.py
+++ usefull_features.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 
-def group_by(data, by, tol=1e-6):
+def group_by(data, by, method='diff', crit=1e-6):
     tmp = data.copy()
     tmp.sort(order=by)
-    ii = [0] + (np.where(np.diff(tmp[by]) >= tol)
+    funcs = {'diff': lambda x: np.diff(
+        x), 'ratio': lambda x: (x/np.roll(x, 1))[1:]}
+    ii = [0] + (np.where(funcs[method](tmp[by]) >= crit)
                 [0]+1).tolist() + [len(tmp[by])]
     groups = []
     for i in range(len(ii)-1):
@@ -17,26 +19,29 @@ def runningMeanFast(x, N):
     rez[:N//2] = rez[N//2]
     return rez
 
+
 def splitByN(l, n):
-    if n>=len(l):
+    if n >= len(l):
         return [l]
-    nn=len(l)//n
-    rez=[]
+    nn = len(l)//n
+    rez = []
     for i in range(nn):
         rez.append(l[i*n:(i+1)*n])
-    if len(l)%n:
+    if len(l) % n:
         rez.append(l[nn*n:])
     return rez
 
+
 def splitByNseq(s, n):
-    while sum(n)+n[-1]<=len(s):
+    while sum(n)+n[-1] <= len(s):
         n.append(n[-1])
-    pos=[sum(n[:i+1]) for i in range(len(n))]
-    pos.insert(0,0)
-    rez=[]
+    pos = [sum(n[:i+1]) for i in range(len(n))]
+    pos.insert(0, 0)
+    rez = []
     for i in range(len(pos)-1):
         rez.append(s[pos[i]:pos[i+1]].strip())
     return rez
+
 
 class ConsoleProgress(object):
     def __init__(self, total, scaleLength=50, text='Progress=',
@@ -50,20 +55,22 @@ class ConsoleProgress(object):
 
     def redraw(self):
         if self.chekRedraw(self.current):
-            print(progress(self.current, self.total, self.scaleLength, self.text, self.symbol), end='')#, flush=True)
-        self.current+=1
+            print(progress(self.current, self.total, self.scaleLength,
+                           self.text, self.symbol), end='')  # , flush=True)
+        self.current += 1
 
     def finalize(self):
-        print(progress(self.total, self.total, self.scaleLength, self.text, self.symbol))
+        print(progress(self.total, self.total,
+                       self.scaleLength, self.text, self.symbol))
 
 
 def progress(cur, total, scaleLength=20, text='Progress=', symbol='-#'):
-    if total!=0:
-        prog=int(100.*cur/total)
+    if total != 0:
+        prog = int(100.*cur/total)
     else:
-        prog=100
-    rez='\r{0}{1:4}% '.format(text, prog)
-    numD=int(prog*scaleLength/100.)
-    rez+=symbol[1]*numD+symbol[0]*(scaleLength-numD)
-    rez+=' Total={0:10}, current={1:10}'.format(total,cur)
+        prog = 100
+    rez = '\r{0}{1:4}% '.format(text, prog)
+    numD = int(prog*scaleLength/100.)
+    rez += symbol[1]*numD+symbol[0]*(scaleLength-numD)
+    rez += ' Total={0:10}, current={1:10}'.format(total, cur)
     return rez
